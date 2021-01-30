75c9ddc877f52e257443f313f27b17ceb9d06985
leocjj <leocj@hotmail.com>
2021-01-30 11:16:39 -0500

    Create 0-lockboxes.py

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" 0x00. Lockboxes Task 0 """
+
+
+def canUnlockAll(boxes):
+    """  Determines if all the boxes can be opened. """
+    list_of_unique_keys = set()
+    number_of_keys = len(list_of_unique_keys)
+
+    for i, keys in enumerate(boxes):
+        temp = set(keys)
+        print(i, keys, list_of_unique_keys, temp)
+        if temp - list_of_unique_keys:
+            list_of_unique_keys |= temp
+        else:
+            if len(list_of_unique_keys) + 1 == len(boxes):
+                return True
+            else:
+                return False
+        if len(list_of_unique_keys) + 1 == len(boxes):
+            return True
+    return False
