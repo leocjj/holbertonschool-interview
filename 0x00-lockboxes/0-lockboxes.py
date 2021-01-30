bc94eeb508abcd77e6d0163e5a323578b018166d
leocjj <leocj@hotmail.com>
2021-01-30 15:10:14 -0500

    Update 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,6 +5,25 @@
 def canUnlockAll(boxes):
     """  Determines if all the boxes can be opened. """
     list_of_unique_keys = set()
+    keys = set(boxes[0])
+
+    while True:
+        temp = keys - list_of_unique_keys
+        # print(i, keys, list_of_unique_keys, temp)
+        if not temp:
+            return False
+        keys = set()
+        for i in temp:
+            if 0 < i < len(boxes):
+                list_of_unique_keys |= {i}
+                keys |= set(boxes[i])
+        if len(list_of_unique_keys) + 1 == len(boxes):
+            return True
+
+
+def canUnlockAll2(boxes):
+    """  Determines if all the boxes can be opened. """
+    list_of_unique_keys = set()
 
     for i, keys in enumerate(boxes):
         temp = set(keys)
