1f7e274273ddd812f7aa945c2f2f37579b623d79
leocjj <leocj@hotmail.com>
2021-01-30 15:11:16 -0500

    Task 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,7 +9,6 @@ def canUnlockAll(boxes):
 
     while True:
         temp = keys - list_of_unique_keys
-        # print(i, keys, list_of_unique_keys, temp)
         if not temp:
             return False
         keys = set()
@@ -19,22 +18,3 @@ def canUnlockAll(boxes):
                 keys |= set(boxes[i])
         if len(list_of_unique_keys) + 1 == len(boxes):
             return True
-
-
-def canUnlockAll2(boxes):
-    """  Determines if all the boxes can be opened. """
-    list_of_unique_keys = set()
-
-    for i, keys in enumerate(boxes):
-        temp = set(keys)
-        # print(i, keys, list_of_unique_keys, temp)
-        if temp - list_of_unique_keys:
-            list_of_unique_keys |= temp
-        else:
-            if len(list_of_unique_keys) + 1 == len(boxes):
-                return True
-            else:
-                return False
-        if len(list_of_unique_keys) + 1 == len(boxes):
-            return True
-    return False
