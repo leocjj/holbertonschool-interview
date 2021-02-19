f92df984a2c8c25b469518db3ba1aabd373440de
leocjj <leocj@hotmail.com>
2021-02-18 20:00:37 -0500

    Task 0

---
+++ 0x03-minimum_operations/0-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4   # Correct answer is 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 9   # Correct answer is 6
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12   # Correct answer is 7
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" 0x00. Minimum operations """
+
+
+def gcd(num):
+    """
+    Calculates the maximum divisor of num
+    :param num: integer
+    :return: the maximum divisor of num
+    """
+    if not isinstance(num, int) or num < 1:
+        return None
+    if num == 2:
+        return 1
+    for divisor in range(int(num / 2) + 1, 0, -1):
+        if (num % divisor) == 0:
+            return divisor
+
+def minOperations(n):
+    """
+    In a text file, there is a single character H. Your text editor can execute
+    only two operations in this file: Copy All and Paste. Given a number n,
+    calculate the fewest number of operations needed to result in exactly n H
+    characters in the file.
+    :param n: given number.
+    :return: Returns an integer, 0 if n is impossible to achieve.
+    """
+    n *= 2
+    if not isinstance(n, int) or n < 1:
+        print(0)
+
+    divs = []
+    while n > 1:
+        divs.append(int(gcd(n)))
+        n = divs[-1]
+    result = 0
+    for i, _ in enumerate(divs):
+        if i == len(divs) - 1:
+            break
+        result += int(divs[i] / divs[i + 1])
+
+    return result
