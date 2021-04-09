1dfaecefbf37698ba227c5899845063032890199
leocjj <leocj@hotmail.com>
2021-04-09 00:43:24 -0500

    Task 0

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+0x09. UTF-8 Validation
+A character in UTF-8 can be 1 to 4 bytes long.
+The data set can contain multiple characters.
+The data will be represented by a list of integers.
+Each integer represents 1 byte of data.
+"""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+    :param data: list of integers
+    :return: True if data is a valid UTF-8 encoding, else return False.
+    """
+
+    if not data or not isinstance(data, list):
+        return False
+    for i in data:
+        if not isinstance(i, int):
+            return False
+
+    data_size = len(data)
+    j = 0
+    while j < data_size:
+        if (data[j] & int('10000000', 2)) == 0:
+            bytes_to_check = 0
+        elif (data[j] & int('11100000', 2)) == int('11000000', 2):
+            bytes_to_check = 1
+        elif (data[j] & int('11110000', 2)) == int('11100000', 2):
+            bytes_to_check = 2
+        elif (data[j] & int('11111000', 2)) == int('11110000', 2):
+            bytes_to_check = 3
+        else:
+            return False
+        if bytes_to_check > data_size - j:
+            return False
+        for i in range(1, bytes_to_check + 1):
+            if (data[j + i] & int('11000000', 2)) == int('10000000', 2):
+                continue
+            else:
+                return False
+        j += bytes_to_check + 1
+    return True
---
+++ 0x09-utf8_validation/README.md
@@ -0,0 +1,42 @@
+#0x09. UTF-8 Validation
+##0. UTF-8 Validation
+
+Write a method that determines if a given data set represents a valid UTF-8 encoding.
+
+Prototype: def validUTF8(data)
+
+Return: True if data is a valid UTF-8 encoding, else return False
+
+A character in UTF-8 can be 1 to 4 bytes long
+The data set can contain multiple characters
+The data will be represented by a list of integers
+Each integer represents 1 byte of data, therefore you only need to handle the 8 least significant bits of each integer
+
+## Requirementes - General
+
+    Allowed editors: vi, vim, emacs
+    All your files will be interpreted/compiled on Ubuntu 14.04 LTS using python3 (version 3.4.3)
+    All your files should end with a new line
+    The first line of all your files should be exactly #!/usr/bin/python3
+    A README.md file, at the root of the folder of the project, is mandatory
+    Your code should be documented
+    Your code should use the PEP 8 style (version 1.7.x)
+    All your files must be executable
+
+
+## Built With
+
+* Windows 10
+* Python 3.6
+* Pycharm 2020.2
+* Pycharm style
+
+## Author
+
+**Leonardo Calderon J.** - *Initial work* 
+
+## [LeoCJJ](https://github.com/leocjj)
+
+[Web Page](http://leocjj.tech)
+
+2021
