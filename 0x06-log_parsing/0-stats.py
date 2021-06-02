854c76cba24a4d2e5ebd43c1cb27c7887b330d36
leocjj <leocj@hotmail.com>
2021-06-02 10:12:28 -0500

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,44 +1,45 @@
 #!/usr/bin/python3
-"""
-0x00. Minimum operations
-Script that reads stdin line by line and computes metrics
-Input: <IP> - [<date>] "GET /projects/260 HTTP/1.1" <stat code> <file size>
-After every 10 lines and/or a keyboard interruption (CTRL + C),
-print these statistics from the beginning.
-"""
+"""Program that reads stdin line by line and computes metrics"""
 from sys import stdin
 
 
-total_size = 0
-stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-         '404': 0, '405': 0, '500': 0}
+statusCode = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+totalSize = 0
 
 
-def print_stats():
-    """
-    Print these statistics from the beginning
-    :return: None
-    """
-
-    print("File size: {}".format(total_size))
-    for key, value in sorted(stats.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
+def printStat():
+    """Function that print log accumulately"""
+    print("File size: {}".format(totalSize))
+    for x in sorted(statusCode.keys()):
+        if statusCode[x]:
+            print("{}: {}".format(x, statusCode[x]))
 
 
 if __name__ == "__main__":
-    line_counter = 0
+    ct = 0
     try:
-        for line in stdin:
-            args = line.split()
-            total_size += int(args[-1])
-            if args[-2] in stats:
-                stats.update({str(args[-2]): stats.get(args[-2]) + 1})
-            line_counter += 1
-            if line_counter == 10:
-                print_stats()
-                line_counter = 0
+        for y in stdin:
+            try:
+                item = y.split()
+                totalSize += int(item[-1])
+                if item[-2] in statusCode:
+                    statusCode[item[-2]] += 1
+            except:
+                pass
+            if ct == 9:
+                printStat()
+                ct = -1
+            ct += 1
     except KeyboardInterrupt:
-        print_stats()
+        printStat()
         raise
-    print_stats()
+    printStat()
