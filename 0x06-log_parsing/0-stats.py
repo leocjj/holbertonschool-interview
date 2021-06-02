0ccee060a97420c80dc6b67c8bf925b7a55d43a6
leocjj <leocj@hotmail.com>
2021-06-02 08:48:25 -0500

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 """
 0x00. Minimum operations
 Script that reads stdin line by line and computes metrics
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+Input: <IP> - [<date>] "GET /projects/260 HTTP/1.1" <stat code> <file size>
 After every 10 lines and/or a keyboard interruption (CTRL + C),
 print these statistics from the beginning.
 """
@@ -16,16 +16,18 @@ def print_stats(total_size, stats):
         if value > 0:
             print("{}: {}".format(key, value))
 
+
 if __name__ == "__main__":
     total_size = 0
     line_counter = 0
-    stats = OrderedDict({'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0})
+    stats = OrderedDict({'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                         '404': 0, '405': 0, '500': 0})
     try:
         for line in sys.stdin:
-            arguments = line.split()
-            if len(arguments) >= 2 and arguments[-2] in stats:
-                stats.update({str(arguments[-2]): stats.get(arguments[-2]) + 1})
-                total_size += int(arguments[-1])
+            args = line.split()
+            if len(args) >= 2 and args[-2] in stats:
+                stats.update({str(args[-2]): stats.get(args[-2]) + 1})
+                total_size += int(args[-1])
             line_counter += 1
             if line_counter == 10:
                 print_stats(total_size, stats)
