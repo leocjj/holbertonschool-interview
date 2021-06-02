12ec21a66a91ab8fe003d99f5519d6df638c74d4
leocjj <leocj@hotmail.com>
2021-06-02 09:15:51 -0500

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import sys
-from collections import OrderedDict
 """
 0x00. Minimum operations
 Script that reads stdin line by line and computes metrics
@@ -11,6 +10,13 @@ print these statistics from the beginning.
 
 
 def print_stats(total_size, stats):
+    """
+    Print these statistics from the beginning
+    :param total_size: Total file size
+    :param stats: dictionary with status code and counters.
+    :return: None
+    """
+
     print("File size: {}".format(total_size))
     for key, value in stats.items():
         if value > 0:
@@ -20,8 +26,8 @@ def print_stats(total_size, stats):
 if __name__ == "__main__":
     total_size = 0
     line_counter = 0
-    stats = OrderedDict({'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                         '404': 0, '405': 0, '500': 0})
+    stats = dict({'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                 '404': 0, '405': 0, '500': 0})
     try:
         for line in sys.stdin:
             args = line.split()
