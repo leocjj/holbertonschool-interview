147ba9f993f4d7f779ad7de6c2d1352d405d5e9f
leocjj <leocj@hotmail.com>
2021-06-02 10:04:56 -0500

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python3
 """
-Write a script that reads stdin line by line and computes metrics
+0x00. Minimum operations
+Script that reads stdin line by line and computes metrics
+Input: <IP> - [<date>] "GET /projects/260 HTTP/1.1" <stat code> <file size>
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning.
 """
-import sys
+from sys import stdin
+
 
 total_size = 0
-counter = 0
-codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-dict_counter = {'200': 0, '301': 0,
-                '400': 0, '401': 0,
-                '403': 0, '404': 0,
-                '405': 0, '500': 0}
+stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+         '404': 0, '405': 0, '500': 0}
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 2:
-            code = line_list[-2]
-            size = line_list[-1]
-            if code in codes:
-                dict_counter[code] += 1
-            total_size += int(size)
-            counter += 1
 
-        if counter == 10:
-            print("File size: {:d}".format(total_size))
-            for k, v in sorted(dict_counter.items()):
-                if v != 0:
-                    print("{}: {:d}".format(k, v))
-            counter = 0
+def print_stats():
+    """
+    Print these statistics from the beginning
+    :return: None
+    """
 
-except Exception:
-    pass
-finally:
     print("File size: {}".format(total_size))
-    for k, v in sorted(dict_counter.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
+    for key, value in sorted(stats.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
+
+
+if __name__ == "__main__":
+    line_counter = 0
+    try:
+        for line in stdin:
+            args = line.split()
+            if len(args) >= 2 and args[-2] in stats:
+                stats.update({str(args[-2]): stats.get(args[-2]) + 1})
+                total_size += int(args[-1])
+            line_counter += 1
+            if line_counter == 10:
+                print_stats()
+                line_counter = 0
+    except KeyboardInterrupt:
+        pass
+    print_stats()
