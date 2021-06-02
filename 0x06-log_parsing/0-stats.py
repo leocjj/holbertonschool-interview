94ddee8f3c8c668237bd3cfd40cf2aa60c0e6899
leocjj <leocj@hotmail.com>
2021-06-02 09:59:40 -0500

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,45 +1,39 @@
 #!/usr/bin/python3
-"""Program that reads stdin line by line and computes metrics"""
-from sys import stdin
+"""
+Write a script that reads stdin line by line and computes metrics
+"""
+import sys
 
+total_size = 0
+counter = 0
+codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+dict_counter = {'200': 0, '301': 0,
+                '400': 0, '401': 0,
+                '403': 0, '404': 0,
+                '405': 0, '500': 0}
 
-statusCode = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-totalSize = 0
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 2:
+            code = line_list[-2]
+            size = line_list[-1]
+            if code in codes:
+                dict_counter[code] += 1
+            total_size += int(size)
+            counter += 1
 
+        if counter == 10:
+            print("File size: {:d}".format(total_size))
+            for k, v in sorted(dict_counter.items()):
+                if v != 0:
+                    print("{}: {:d}".format(k, v))
+            counter = 0
 
-def printStat():
-    """Function that print log accumulately"""
-    print("File size: {}".format(totalSize))
-    for x in sorted(statusCode.keys()):
-        if statusCode[x]:
-            print("{}: {}".format(x, statusCode[x]))
-
-
-if __name__ == "__main__":
-    ct = 0
-    try:
-        for y in stdin:
-            try:
-                item = y.split()
-                totalSize += int(item[-1])
-                if item[-2] in statusCode:
-                    statusCode[item[-2]] += 1
-            except:
-                pass
-            if ct == 9:
-                printStat()
-                ct = -1
-            ct += 1
-    except KeyboardInterrupt:
-        printStat()
-        raise
-    printStat()
+except Exception:
+    pass
+finally:
+    print("File size: {}".format(total_size))
+    for k, v in sorted(dict_counter.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
