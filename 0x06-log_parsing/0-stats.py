fd001d2c92ba89059298b9b649663fdbca977b0f
leocjj <leocj@hotmail.com>
2021-06-02 09:35:13 -0500

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 import sys
+"""
+0x00. Minimum operations
+Script that reads stdin line by line and computes metrics
+Input: <IP> - [<date>] "GET /projects/260 HTTP/1.1" <stat code> <file size>
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning.
+"""
+total_size = 0
+stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+         '404': 0, '405': 0, '500': 0}
 
 
-def print_stats(size, stat):
+def print_stats():
     """
     Print these statistics from the beginning
-    :param size: Total file size
-    :param stat: dictionary with status code and counters.
     :return: None
     """
 
-    print("File size: {}".format(size))
-    for key, value in stat.items():
+    print("File size: {}".format(total_size))
+    for key, value in sorted(stats.items()):
         if value > 0:
             print("{}: {}".format(key, value))
 
 
 if __name__ == "__main__":
-    total_size = 0
     line_counter = 0
-    stats = dict({'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                 '404': 0, '405': 0, '500': 0})
     try:
         for line in sys.stdin:
             args = line.split()
@@ -29,8 +34,9 @@ if __name__ == "__main__":
                 total_size += int(args[-1])
             line_counter += 1
             if line_counter == 10:
-                print_stats(total_size, stats)
+                print_stats()
                 line_counter = 0
     except KeyboardInterrupt:
-        print_stats(total_size, stats)
+        print_stats()
         raise
+    print_stats()
