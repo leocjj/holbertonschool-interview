0351367c752d916e8af302c2cca20b694de46a6d
leocjj <leocj@hotmail.com>
2021-06-25 10:00:26 -0500

    Update 0-main.py

--- 0x09-utf8_validation/0-main.py
+++ 0x09-utf8_validation/0-main.py
@@ -6,10 +6,21 @@ Main file for testing
 validUTF8 = __import__('0-validate_utf8').validUTF8
 
 data = [65]
-print(validUTF8(data))
-
+print(validUTF8(data))      # True
 data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-print(validUTF8(data))
-
+print(validUTF8(data))      # True
 data = [229, 129, 127, 256]
-print(validUTF8(data))
+print(validUTF8(data))      # False
+data = [300, 301, 302]
+print(validUTF8(data))      # True
+data = []
+print(validUTF8(data))      # True
+data = [256]
+print(validUTF8(data))      # True
+data = [0]
+print(validUTF8(data))      # True
+data = [235, 140]
+print(validUTF8(data))      # False
+data = [345, 467]
+print(validUTF8(data))      # False
+
