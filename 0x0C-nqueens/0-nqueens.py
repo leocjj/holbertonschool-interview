710f5e3d30d58fe426a9a31e298e63ad4b79f3f5
leocjj <leocj@hotmail.com>
2021-07-22 23:12:21 -0500

    Update 0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
 """
-nqueens problem.
+Program that solves the N queens problem
 """
+
+
 import sys
 
 
-def solve(N, i=0, a=[], b=[], c=[]):
-    """ Find partial solutions recursively """
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+
+def backtracking(N, i=0, a=[], b=[], c=[]):
+    """Generates backtracking solutions for N queens problem"""
     if i < N:
         for j in range(N):
             if j not in a and i+j not in b and i-j not in c:
-                for solution in solve(N, i+1, a+[j], b+[i+j], c+[i-j]):
+                for solution in backtracking(N, i+1, a+[j], b+[i+j], c+[i-j]):
                     yield solution
     else:
         yield a
 
-
-nq = None
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
-try:
-    nq = int(sys.argv[1])
-except ValueError:
-    print('N must be a number')
-    exit(1)
-if not isinstance(nq, int):
-    print('N must be a number')
-    exit(1)
-if nq < 4:
-    print('N must be at least 4')
-    exit(1)
-
-for sol in solve(nq):
-    answer = [[col, row] for col, row in enumerate(sol)]
+for solution in backtracking(N):
+    answer = [[col, row] for col, row in enumerate(solution)]
     print(answer)
