8550f21a0291739eab55a5cd5d072b92d40f5063
leocjj <leocj@hotmail.com>
2021-07-22 22:58:48 -0500

    Update 0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -3,26 +3,8 @@
 import sys
 
 
-def init():
-    nq = None
-    if len(sys.argv) != 2:
-        print('Usage: nqueens N')
-        exit(1)
-    try:
-        nq = int(sys.argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
-    if not isinstance(nq, int):
-        print('N must be a number')
-        exit(1)
-    if nq < 4:
-        print('N must be at least 4')
-        exit(1)
-    return nq
-
-
 def solve(N, i=0, a=[], b=[], c=[]):
+    """ Find partial solutions recursively """
     if i < N:
         for j in range(N):
             if j not in a and i+j not in b and i-j not in c:
@@ -32,8 +14,22 @@ def solve(N, i=0, a=[], b=[], c=[]):
         yield a
 
 
-if __name__ == '__main__':
-    nqueens = init()
-    for solution in solve(nqueens):
-        answer = [[col, row] for col, row in enumerate(solution)]
-        print(answer)
+nq = None
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+try:
+    nq = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
+    exit(1)
+if not isinstance(nq, int):
+    print('N must be a number')
+    exit(1)
+if nq < 4:
+    print('N must be at least 4')
+    exit(1)
+
+for sol in solve(nq):
+    answer = [[col, row] for col, row in enumerate(sol)]
+    print(answer)
