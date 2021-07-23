50b4d8d59401146d38407a9412379769a4a27041
leocjj <leocj@hotmail.com>
2021-07-22 23:17:48 -0500

    Task 0

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,41 +1,39 @@
 #!/usr/bin/python3
-"""Finds the number of queens that can be placed on a board
-   without being under attack"""
+"""
+nqueens problem.
+"""
 import sys
 
-N = sys.argv
-if len(N) != 2:
-    print("Usage: nqueens N")
+
+def solve(N, i=0, a=[], b=[], c=[]):
+    """
+    Find partial solutions recursively
+    """
+    if i < N:
+        for j in range(N):
+            if j not in a and i+j not in b and i-j not in c:
+                for solution in solve(N, i+1, a+[j], b+[i+j], c+[i-j]):
+                    yield solution
+    else:
+        yield a
+
+
+nq = None
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
     exit(1)
-N = int(N[1])
-if not isinstance(N, int):
-    print("N must be a number")
+try:
+    nq = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
     exit(1)
-if N < 4:
-    print("N must be at least 4")
+if not isinstance(nq, int):
+    print('N must be a number')
+    exit(1)
+if nq < 4:
+    print('N must be at least 4')
     exit(1)
 
-
-def under_attack(col, queens):
-    return col in queens or \
-           any(abs(col - x) == len(queens)-i for i, x in enumerate(queens))
-
-
-def solve(n):
-    solutions = [[]]
-    for row in range(n):
-        solutions = [solution+[i]
-                     for solution in solutions
-                     for i in range(N)
-                     if not under_attack(i, solution)]
-    return solutions
-
-
-for answer in solve(N):
-    A = list(enumerate(answer, start=0))
-    i = 0
-    for t in A:
-        t = list(t)
-        A[i] = t
-        i += 1
-    print(A)
+for sol in solve(nq):
+    answer = [[col, row] for col, row in enumerate(sol)]
+    print(answer)
