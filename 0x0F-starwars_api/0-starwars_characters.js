1b9a36ce2f7e9d82c46d3520942d496d573e6cba
leocjj <leocj@hotmail.com>
2021-08-12 22:43:09 -0500

    Create 0-starwars_characters.js

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/* export NODE_PATH=/usr/lib/node_modules */
+const request = require('request');
+const baseURL = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(baseURL, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const characters = JSON.parse(body).characters;
+
+  for (let i = 0; i < characters.length; i++) {
+    await new Promise(function (resolve, reject) {
+      request(characters[i], function (err, res, bod) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          console.log(JSON.parse(bod).name);
+          resolve();
+        }
+      });
+    });
+  }
+});
