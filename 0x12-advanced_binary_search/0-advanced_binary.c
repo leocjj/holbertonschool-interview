d3f6514ce2413ccd5beffb2bf9ee5d6317bfc5d3
leocjj <leocj@hotmail.com>
2021-09-02 22:01:45 -0500

    Update 0-advanced_binary.c

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -2,8 +2,8 @@
 /**
 * advanced_binary - searches for a value in a sorted array of integers.
 * @array: pointer to the first element of the array to search in
-* @size: number of elements in array
-* @value: value to search for
+* @size: number of elements in array.
+* @value: value to search for.
 * Return: index where value is located.
 * If value is not present in array or if array is NULL, return -1
 */
