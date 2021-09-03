bf5e6003f1070a89e11039d7e47b7fe53300214f
leocjj <leocj@hotmail.com>
2021-09-02 22:01:18 -0500

    Update search_algos.h

--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -1,6 +1,8 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-int advanced_binary(int *array, size_t size, int value);
+#include <stdio.h>
+#include <stdlib.h>
 
+int advanced_binary(int *array, size_t size, int value);
 #endif
