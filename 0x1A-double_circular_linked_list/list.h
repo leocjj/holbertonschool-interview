4425e5c2b8834db81ad25201ada8b1bdf716cde9
leocjj <leocj@hotmail.com>
2021-11-04 16:06:34 -0500

    Create list.h

---
+++ 0x1A-double_circular_linked_list/list.h
@@ -0,0 +1,32 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Data structure */
+/**
+ * struct List - doubly linked list
+ * @str: string - (malloc'ed string)
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct List
+{
+	char *str;
+	struct List *prev;
+	struct List *next;
+} List;
+
+/* Mandatory functions */
+List *add_node_end(List **list, char *str);
+List *add_node_begin(List **list, char *str);
+
+/* Auxiliary functions */
+List *create_node(char *str);
+
+#endif
