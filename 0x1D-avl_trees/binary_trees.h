dc000f6c3082f277c2ed792c0e56ea7c6f780858
leocjj <leocj@hotmail.com>
2021-11-25 20:52:34 -0500

    Create binary_trees.h

---
+++ 0x1D-avl_trees/binary_trees.h
@@ -0,0 +1,43 @@
+#ifndef felipeserna
+#define felipeserna
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <limits.h>
+
+/* Basic Binary Tree */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+
+/* Given print function */
+void binary_tree_print(const binary_tree_t *tree);
+
+/* Function from Binary Trees project (Foundations) */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Mandatory function */
+int binary_tree_is_avl(const binary_tree_t *tree);
+
+/* Auxiliary functions */
+int is_valid_avl(const binary_tree_t *tree, int min, int max, int *height);
+
+#endif
